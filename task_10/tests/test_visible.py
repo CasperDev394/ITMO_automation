d3a810178b301d6177e4e08f70120a9fa7a72c00
Борис <bomen.394@gmail.com>
2023-02-01 14:25:00 +0300

    task 9 and 10 upd

--- task_10/tests/test_visible.py
+++ task_10/tests/test_visible.py
@@ -3,12 +3,12 @@ from pages.accordion_page import Accordion
 import time
 
 
-def test_visible_btn_sidebar(browser):
-    elements_page = ElementsPage(browser)
-    elements_page.visit()
-    # elements_page.btn_sidebar_first.click()
-    time.sleep(3)
-    assert elements_page.btn_sidebar_first_textbox.visible()
+# def test_visible_btn_sidebar(browser):
+#     elements_page = ElementsPage(browser)
+#     elements_page.visit()
+#     # elements_page.btn_sidebar_first.click()
+#     time.sleep(3)
+#     assert elements_page.btn_sidebar_first_textbox.visible()
 
 
 def test_not_visible_btn_sidebar(browser):
